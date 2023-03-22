abdcd745e5a0cad9f0ef3872c7beb4c70e3ea202
Jesus Moran Espinoza <jame291104@gmail.com>
2023-03-21 23:42:05 -0500

    home finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,47 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/header.css'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export const Header = () => {
   return (
-    <div className='encabezado'>
-       <div className="logo">
+    <header>
+       <Navbar bg="" expand="lg">
+      <Container fluid>
+          <div className="logo">
             <img src="./img/logo1.png" alt="logo"/>
-        </div>
-        <nav>
-            <ul className="links">
-                <li><Link className="text-decoration-none text-light" to='/'>Inicio</Link></li>
-                <li><Link className="text-decoration-none text-light" to='/Ofertas'>Ofertas</Link></li>
-                <li><Link className="text-decoration-none text-light" to='/AboutUs'>Nosotros</Link></li>
-            </ul>
-        </nav>
-        
-        
-        
-    </div>
+          </div>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link><Link className="text-decoration-none text-light mx-3" to='/'>Inicio</Link></Nav.Link>
+            <Nav.Link><Link className="text-decoration-none text-light mx-3" to='/Ofertas'>Ofertas</Link></Nav.Link>
+            <Nav.Link><Link className="text-decoration-none text-light mx-3" to='/AboutUs'>Nosotros</Link></Nav.Link>
+            <Nav.Link className="text-decoration-none text-light mx-3">Compras <i class="bi bi-bag-check-fill"></i></Nav.Link>
+            <NavDropdown className='bg-light text-light' title="Vender" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Mis Productos</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>    
+    </header>
+    
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import '../styles/home.css'
+import { useEffect, useState } from 'react';
 import { Header } from "./Header";
 import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
 
 export const Home = () => {
+  const [products, setProducts] = useState([])
+
+  const getApi = async () => {
+      try {
+        const res = await fetch('https://fakestoreapi.com/products')
+        const data = await res.json();
+
+        setProducts(data)
+      } catch (error) {
+        console.log("uy parce")
+      }
+  }
+
+  useEffect(() => {
+   getApi()
+  }, [])
+  
   return (
     <main>
       <Header/>
@@ -68,7 +88,25 @@ export const Home = () => {
              </Carousel.Caption>
            </Carousel.Item>
         </Carousel> 
-        <div>Home</div>
+        <div className='my-5 row justify-content-center  mx-0'>
+          {
+            products.map((product)=>
+            <Card className='m-4 px-0' key={product.id} style={{ width: '18rem' }}>
+            <Card.Img  className='py-3' variant="top" src={product.image} 
+            width={200}
+            height={300}
+            />
+            <Card.Body>
+              <Card.Title>{product.title}</Card.Title>
+              <Card.Text>Stock disponible</Card.Text>
+              <Card.Text className='h3 price'>${product.price}</Card.Text>
+            </Card.Body>
+            <button className='buy'>Comprar <i class="bi bi-cart-check-fill"></i></button>
+          </Card>
+               )
+          }
+           
+        </div>
     </main>
     
   )
